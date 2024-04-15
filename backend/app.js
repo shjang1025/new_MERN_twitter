84e69b5a385d5260f0abc84db17a6bcf17c88da3
shjang1025 <shjang1025@gmail.com>
2024-04-15 00:12:20 -0700

    Backend Tweets

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,6 @@
-// const express = require('express');
-// const path = require('path');
-// const cookieParser = require('cookie-parser');
-// const logger = require('morgan');
 
-// const usersRouter = require('./routes/api/users');
-
-// const app = express();
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/users', usersRouter);
-
-// module.exports = app;
 require('./models/User');
+require('./models/Tweet'); 
 require('./config/passport'); 
 const passport = require('passport'); 
 
